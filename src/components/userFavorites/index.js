55926d65e0c6fae19e42749a0948b57528656cf1
BryanBeasley <78266471+BryanBeasley@users.noreply.github.com>
2021-05-21 11:13:16 -0400

    styling userpage

--- src/components/userFavorites/index.js
+++ src/components/userFavorites/index.js
@@ -9,13 +9,22 @@ import {
     CardBody,
   } from "reactstrap";
 
+  const styles = {
+    favSpacing: {
+       marginLeft: 20,
+       marginRight: 20,
+       marginTop: 20,
+       marginBottom: 20
+     }
+   };
+
 
 function UserFavorites() {
   return (
       
           <CardGroup>
               <h4>My Favorite Recipes</h4>
-            <Card> 
+            <Card style={ styles.favSpacing }> 
              <CardBody body inverse color="primary">
                 <CardTitle tag="h5"></CardTitle>
                 <CardSubtitle tag="h6" className="mb-2 text-muted">Comments why this is Favorite
--- src/components/userProfile/index.js
+++ src/components/userProfile/index.js
@@ -1,19 +1,28 @@
 import React from "react";
+import {  Container, Row, Col, Card, CardText, CardBody, } from 'reactstrap';
+
 import "./userProfile.scss";
 
 
 function UserProfile() {
   return (
   
-    <div>
-      <div className="user-img" >
-        <img src="https://image.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
-        />
-      </div>
-      <div className="user-name" > 
-        <h4>Beezle Bryan</h4>
-      </div>
-    </div>
+    <Container>
+      <Row  xs="2">
+        <Col sm={{ size: 6, order: 2, offset: 1 }}>
+
+          <Card style={{borderRadius: 50 }}>
+        <img style={{borderTopRightRadius: 50, borderTopLeftRadius: 50 }} className="user-img" src="https://image.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" alt="User Avatar" />
+         <CardBody>
+          <CardText>Beezle Bryan</CardText>
+          </CardBody>
+        </Card>
+        
+      </Col>
+      </Row>
+      
+      
+    </Container>
  
   );
 }
