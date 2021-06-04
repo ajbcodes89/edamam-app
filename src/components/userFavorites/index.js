af376a591b850b043a9f18d6fe7f475fa6b07461
BryanBeasley <78266471+BryanBeasley@users.noreply.github.com>
2021-06-03 19:38:04 -0500

    final days

--- src/components/userFavorites/index.js
+++ src/components/userFavorites/index.js
@@ -1,66 +1,44 @@
-import React, {useState, useEffect } from "react";
-// import "./userFavorites.scss";
+import React from "react";
+
 import {
-    Container, Col, Row,
-    Card,
-    CardTitle,
-    CardImg,
-    CardText,
-    CardGroup,
-    CardSubtitle,
-    CardBody,
-  } from "reactstrap";
+  Card,
+  CardTitle,
+  CardImg,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap";
 
-  const styles = {
-    favSpacing: {
-       marginLeft: 20,
-       marginRight: 20,
-       marginTop: 20,
-       marginBottom: 20
-     }
-   };
+const styles = {
+  favSpacing: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+};
 
+const UserFavoriteCard = (props) => {
+  const { info } = props;
 
-function UserFavorites() {
-  const [favorites, setFavorites] = useState([])
-  
-  
-  
-  useEffect(() => {
-    fetch("http://localhost:9000/favorites/mine" , {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        Authorization: localStorage.getItem('token'),
-      })
-    })
-      .then(res => (res.json()))
-      .then( json => {
-        setFavorites(json.mine)
-        console.log(json.mine)
-      })
-  }, [])
+  // console.log(info);
 
-  
   return (
-      
-          <CardGroup>
-            
-              <h4>My Favorite Recipes</h4>
-            <Card style={ styles.favSpacing }> 
-             <CardBody body inverse color="primary">
-             <CardImg top width="100%" src={favorites.imageURL} alt="Card image cap" />
-                <CardTitle tag="h5">{favorites.title}</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">Comments why this is Favorite
-                </CardSubtitle>
-                <CardText>
-                </CardText>
-              </CardBody>
-            </Card>
-          </CardGroup>
-        );
-      };
-      
-        
-    export default UserFavorites;
-  
+    <CardGroup>
+      <h4>My Favorite Recipes</h4>
+      <Card style={styles.favSpacing}>
+        <CardBody body inverse color="primary">
+          <CardImg top width="100%" src={info.image} alt="Card image cap" />
+          <CardTitle tag="h5"> {info.label} </CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {info.calories}
+          </CardSubtitle>
+          <CardText></CardText>
+        </CardBody>
+      </Card>
+    </CardGroup>
+  );
+};
+
+export default UserFavoriteCard;
